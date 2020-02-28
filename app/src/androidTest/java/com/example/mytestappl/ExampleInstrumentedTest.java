376e6a26fe9cf06c02451d91267453fb89e2d737
sere995 <serena.giugliano.995@gmail.com>
2020-02-28 11:58:53 +0100

    coommit 1

--- app/src/androidTest/java/com/example/mytestappl/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mytestappl/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.mytestappl;
+/* package com.example.mytestappl;
 
 import android.content.Context;
 
@@ -15,6 +15,8 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+/*
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -23,5 +25,35 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.mytestappl", appContext.getPackageName());
+    }
+}
+*/
+
+
+package com.example.mytestappl;
+
+        import android.content.Context;
+
+        import androidx.test.platform.app.InstrumentationRegistry;
+        import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.testapp", appContext.getPackageName());
     }
 }
